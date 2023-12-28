c62b67fcbc8cde09531d7758a47de63d82788f03
saba070201 <saba07022001@yandex.ru>
2023-12-28 23:10:40 +0700

    add csv script

--- module2/pr4/data_script.py
+++ module2/pr4/data_script.py
@@ -1,6 +1,8 @@
 import sqlite3
-import names 
-import random 
+import names
+import random
+import pandas as pd 
+
 
 def initial_tables(conn: sqlite3.connect, cur):
     """
@@ -16,32 +18,47 @@ def initial_tables(conn: sqlite3.connect, cur):
     )
     """)
     conn.commit()
-def set_data_to_db(conn,cur,qualifications,positions):
+
+
+def set_data_to_db(conn, cur, qualifications, positions):
     """
     Заполнить таблицу в базе данных 
     """
     for i in range(10000):
-        qualification=random.choice(list(qualifications.keys()))
-        position=random.choice(positions)
-        salary=random.choice(qualifications[qualification])
-        cur.execute("""INSERT INTO staff(name,salary,position,qualification) VALUES(?,?,?,?);""",(names.get_full_name(),salary,position,qualification))
+        qualification = random.choice(list(qualifications.keys()))
+        position = random.choice(positions)
+        salary = random.choice(qualifications[qualification])
+        cur.execute("""INSERT INTO staff(name,salary,position,qualification) VALUES(?,?,?,?);""",
+                    (names.get_full_name(), salary, position, qualification))
 
         conn.commit()
 
-def get_data_from_db(conn,cur):
+
+def get_data_from_db(conn, cur):
     """
     Получить данные из базы данных
     """
-    data=cur.execute(""" SELECT * FROM staff;""")
+    data = cur.execute(""" SELECT * FROM staff;""")
     return data.fetchall()
 
-data_sample_qualification = {'Junior': [300,1000],'Middle':[900,5000],'Senior':[3000,10000],'God':[10000,9999999]}
-data_sample_positions=['Backend developer','Frontend developer','Project manager']
-DB_PATH='testdb.db'
+
+data_sample_qualification = {'Junior': [300, 1000], 'Middle': [
+    900, 5000], 'Senior': [3000, 10000], 'God': [10000, 9999999]}
+data_sample_positions = ['Backend developer',
+                         'Frontend developer', 'Project manager']
+DB_PATH = 'testdb.db'
 conn = sqlite3.connect(DB_PATH)
-cur=conn.cursor()
+cur = conn.cursor()
+d=get_data_from_db(conn=conn,cur=cur)
+
 # initial_tables(conn=conn,cur=cur)
 # set_data_to_db(conn=conn,cur=cur,qualifications=data_sample_qualification,positions=data_sample_positions)
 # print(get_data_from_db(conn=conn,curr=cur))
 conn.close()
 
+
+def create_csv(data):
+    for i in data:
+        df=pd.DataFrame({'Id':[i[0] for i in data ],'Name':[i[1] for i in data ],'Salary':[i[2] for i in data ],'Position':[i[3] for i in data ],'Qualification':[i[4] for i in data ]})
+    df.to_csv('staff.csv')
+create_csv(d)
